cf14f65f292f48d4f715ddd6062447a1ad73cbab
David Weppler <david.weppler@icloud.com>
2021-02-07 16:48:12 +0100

    floor

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.post('/api/post', verifyToken, (req, res) => {
             return res.sendStatus(403);
         } else {
             const json = req.headers['json'];
-            let cov = parseCoverage(json);
+            let cov = Math.floor(parseCoverage(json) * 100);
             docRef.update({ "coverage": cov });
             return res.sendStatus(200);
         }
