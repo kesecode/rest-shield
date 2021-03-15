a0089be0dc7cf7a6f4719e6b90fa2586242b3571
David <43249973+kesecode@users.noreply.github.com>
2021-03-15 19:20:50 +0100

    Update rest_connect.sh

--- .github/scripts/rest_connect.sh
+++ .github/scripts/rest_connect.sh
@@ -3,8 +3,6 @@
 COVERAGE=`jq -r '.total.lines.pct' coverage-summary.json`
 COVERAGE=$(echo ${COVERAGE%.*})
 
-echo hallo hallo $COVERAGE
-
 TOKEN=$(curl --location --request POST 'rest.kesecode.io/api/auth' --header 'Authorization: '$USER' '$PASSWORD'' | jq -r '.token')
 
 curl --location --request POST 'rest.kesecode.io/post/kesecode/rest-shield/coverage' --header 'Authorization: Bearer '$TOKEN'' --header 'JSON: {"coverage": '$COVERAGE'}'
