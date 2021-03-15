5104959f92a40338d53e7be40c05583abeaff2e0
David Weppler <david.weppler@icloud.com>
2021-03-15 20:15:28 +0100

    fix

--- .github/scripts/rest_connect.sh
+++ .github/scripts/rest_connect.sh
@@ -5,4 +5,4 @@ COVERAGE=$(echo ${COVERAGE%.*})
 
 TOKEN=$(curl --location --request POST 'rest.kesecode.io/auth' --header 'Authorization: '$USER' '$PASSWORD'' | jq -r '.token')
 
-curl --location --request POST 'rest.kesecode.io/post/kesecode/rest-shield/coverage' --header 'Authorization: Bearer '$TOKEN'' --header 'JSON: {"coverage": '$COVERAGE'}'
+curl --location --request POST 'rest.kesecode.io/post/kesecode/rest-shield/coverage' --header 'Authorization: Bearer '$TOKEN'' --header 'JSON: {"value": '$COVERAGE'}'
