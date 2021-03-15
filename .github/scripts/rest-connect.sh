c09c892c3eff47ab86deebfc6e0063d238c39c26
David <43249973+kesecode@users.noreply.github.com>
2021-03-15 18:15:41 +0100

    Delete .github/scripts directory

--- .github/scripts/rest-connect.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-COVERAGE=`jq -r '.total.lines.pct' coverage-summary.json`
-
-TOKEN=$(curl --location --request POST 'rest.kesecode.io/api/auth' --header 'Authorization: '$USER' '$PASSWORD'' | jq -r '.token')
-
-curl --location --request POST 'rest.kesecode.io/api/post' --header 'Authorization: Bearer '$TOKEN'' --header 'JSON: {"coverage": '$COVERAGE'}'
