e9632d3057dc149282342a17663badb2e3629e2b
David <43249973+kesecode@users.noreply.github.com>
2022-11-25 03:01:40 +0100

    Update rest_connect.sh

--- .github/scripts/rest_connect.sh
+++ .github/scripts/rest_connect.sh
@@ -3,6 +3,9 @@
 COVERAGE=`jq -r '.total.lines.pct' coverage-summary.json`
 COVERAGE=$(echo ${COVERAGE%.*})
 
+echo $POST_URL
+echo $AUTH_URL
+
 TOKEN=$(curl --location --request POST $AUTH_URL --header 'Authorization: '$USER' '$PASSWORD'' | jq -r '.token')
 
 curl --location --request POST $POST_URL --header 'Authorization: Bearer '$TOKEN'' --header 'JSON: {"value": '$COVERAGE'}'
