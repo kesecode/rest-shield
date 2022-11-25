97b9c432e7f033de361973dc399d3fc3868253d7
David <43249973+kesecode@users.noreply.github.com>
2022-11-25 03:08:20 +0100

    Update rest_connect.sh

--- .github/scripts/rest_connect.sh
+++ .github/scripts/rest_connect.sh
@@ -3,9 +3,6 @@
 COVERAGE=`jq -r '.total.lines.pct' coverage-summary.json`
 COVERAGE=$(echo ${COVERAGE%.*})
 
-echo $POST_URL
-echo $AUTH_URL
+TOKEN=$(curl --location --request POST 'rest.davidweppler.dev/auth' --header 'Authorization: '$USER' '$PASSWORD'' | jq -r '.token')
 
-TOKEN=$(curl --location --request POST $AUTH_URL --header 'Authorization: '$USER' '$PASSWORD'' | jq -r '.token')
-
-curl --location --request POST $POST_URL --header 'Authorization: Bearer '$TOKEN'' --header 'JSON: {"value": '$COVERAGE'}'
+curl --location --request POST 'rest.davidweppler.dev/post/kesecode/rest-shield/coverage' --header 'Authorization: Bearer '$TOKEN'' --header 'JSON: {"value": '$COVERAGE'}'
